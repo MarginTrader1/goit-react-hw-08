86be49c2dd8cf881f9a92df1bb3d6af8bed4b218
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-08-17 17:06:16 +0300

    Login Page and Form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 import Layout from "../Layout/Layout";
+import ContactsPage from "../../pages/ContactsPage/ContactsPage";
+import RegistrationPage from "../../pages/RegistrationPage/RegistrationPage";
+import LoginPage from "../../pages/LoginPage/LoginPage";
 
 const App = () => {
    return (
@@ -9,6 +12,9 @@ const App = () => {
          <Layout>
             <Routes>
                <Route path="/" element={<HomePage />} />
+               <Route path="/contacts" element={<ContactsPage />} />
+               <Route path="/register" element={<RegistrationPage />} />
+               <Route path="/login" element={<LoginPage />} />
                <Route path="*" element={<NotFoundPage />} />
             </Routes>
          </Layout>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,37 +1,5 @@
-import ContactList from "../../components/ContactList/ContactList";
-import SearchBox from "../../components/SearchBox/SearchBox";
-import ContactForm from "../../components/ContactForm/ContactForm";
-import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
-import Loader from "../../components/Loader/Loader";
-
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contacts/operations";
-import { selectLoading, selectError } from "../../redux/contacts/selectors";
-import { Toaster } from "react-hot-toast";
-
-import css from "./HomePage.module.css";
-
 const HomePage = () => {
-   const dispatch = useDispatch();
-   const error = useSelector(selectError);
-   const isLoading = useSelector(selectLoading);
-
-   //используем хук useEffect для первой загрузки
-   useEffect(() => {
-      dispatch(fetchContacts());
-   }, [dispatch]);
-
-   return (
-      <div className={css.container}>
-         <ContactForm />
-         <SearchBox />
-         {isLoading ? <Loader /> : <ContactList />}
-         {error && <ErrorMessage />}
-
-         <Toaster position="top-right" reverseOrder={false} />
-      </div>
-   );
+   return <div>Это страничка HomePage</div>;
 };
 
 export default HomePage;
